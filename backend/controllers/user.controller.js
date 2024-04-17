fe568fea71ed2b6ea52412bb8fafe6b5c4f46625
Rafik Hijazy <rafikhz90@gmail.com>
2024-04-17 18:47:28 +0300

    finish add user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,7 +4,20 @@ const addUser=async(req,res)=>{
      
      // const New_user=await userModel.create({firstName:"Rafik",lastName:"Hijazy",email:"Rafik@example.com",password:"Apocalypse55"});
       const user=await userModel.findOne({firstName:"Rafik"});
-      res.status(200).json({user:user})
+      if(user){
+        return res.status(500).json({
+            status:"failed",
+            message:"user already exist"
+        })
+      
+      }
+      else{
+        user.create(req.body)
+        return res.status(201).json({
+            status:"success",
+            message:"user created successfully"
+        })
+    }
     }catch(err){
         console.log(err)
     }
